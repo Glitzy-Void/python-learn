38f7f86823e38844a55334524d6c491c6f06ca36
Glitzy-Void <1774144346@qq.com>
2021-04-12 15:04:35 +0800

    4.12

--- python-learn/snake book/3-4.py
+++ python-learn/snake book/3-4.py
@@ -15,3 +15,40 @@ i=0
 while(i<3):
     print(list_dinner[i]+',please allow me to invite you to dinner.')
     i=i+1
+
+#找到了更大的餐桌，名单内添加新的嘉宾
+print('I can invite more people!')
+list_dinner.insert(0,'Yu')
+list_dinner.insert(1,'Sun')
+list_dinner.append('Xue')
+
+#打印名单
+print(list_dinner)
+
+#重新发出邀请
+i=0
+while(i<6):
+    print(list_dinner[i]+',please allow me to invite you to dinner.')
+    i=i+1
+
+#出现了变故 发出道歉
+print('Only two people can come for dinner.')
+i=0
+while(i<4):
+    print(list_dinner.pop()+",I'm so sorry.")
+    i=i+1
+
+#余下人员名单
+print('List of dinner:'+str(list_dinner))
+
+#发起邀请
+print(list_dinner[0]+','+list_dinner[1]+',please allow me to invite you to dinner.')
+
+#感谢语
+print('Nice dining with you.')
+
+#删除名单
+del list_dinner[0]
+del list_dinner[0]
+
+print(list_dinner)
