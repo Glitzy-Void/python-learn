0a9757328042a9eb56ec39d929560ab402eae0e6
Glitzy-Void <1774144346@qq.com>
2021-04-12 15:04:35 +0800

    4.18

--- c-learn/4.18/4.cpp
+++ c-learn/4.18/4.cpp
@@ -4,7 +4,7 @@ int main(void)
 {
     float c,f;
     printf("输入华氏温度：");
-    scanf("%f",f);
+    scanf("%f",&f);
     c=5.0/9.0*(f-32);
     // 5/9无法被正确数学运算 因为都为int型 要修改成5.0/9.0
     printf("摄氏温度为：%.2f",c);
